21c8c0cbca15869821715f2821fd04920a7ba8ea
Sam Roque-Worcel <sam@roque-worcel.com>
2023-12-08 21:31:18 +0000

    Solve part 2

--- 8/main.py
+++ 8/main.py
@@ -23,18 +23,36 @@ def parse_map(filename):
 # instructions, world = parse_map("input_test.txt")
 instructions, world = parse_map("input.txt")
 
-cur_pos = "AAA"
+starting_positions = []
+for k in world:
+    if k.endswith("A"):
+        starting_positions.append(k)
+
 nb = 0
+cur_pos = list(starting_positions)
+first_reached_z = [-1] * len(cur_pos)
 while True:
     instruction = instructions[nb % len(instructions)]
-    if instruction == "L":
-        cur_pos = world[cur_pos][0]
-    else:
-        cur_pos = world[cur_pos][1]
+    nb_finished = 0
+    for pos, starting_position in enumerate(starting_positions):
+        if instruction == "L":
+            cur_pos[pos] = world[cur_pos[pos]][0]
+        else:
+            cur_pos[pos] = world[cur_pos[pos]][1]
 
-    if cur_pos == "ZZZ":
-        print nb + 1
-        break
+        if cur_pos[pos].endswith('Z'):
+            print("%s reached pos %s" %( pos, cur_pos[pos]))
+            first_reached_z[pos] = nb + 1
+
+    nb_reached_z = 0
+    for x in first_reached_z:
+        if x != -1:
+            nb_reached_z += 1
+    
+    if nb_reached_z == len(cur_pos):
+        print(first_reached_z)
+        print("Grab those numbers and paste them here: https://www.calculatorsoup.com/calculators/math/lcm.php")
+        sys.exit()
 
     nb += 1
 
