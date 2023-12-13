733f2287c106fb7489c682ff5a4f0c38144dd6cb
Sam Joan <sam@roque-worcel.com>
2023-12-13 23:25:21 +1300

    Solve part 12

--- 12/main.py
+++ 12/main.py
@@ -1,15 +1,7 @@
 from pprint import pprint
-from functools import cache
 from itertools import product
+import functools
 import sys
-
-def listToTuple(function):
-    def wrapper(*args):
-        args = [tuple(x) if type(x) == list else x for x in args]
-        result = function(*args)
-        result = tuple(result) if type(result) == list else result
-        return result
-    return wrapper
 
 def get_springs(filename):
     fh = open(filename)
@@ -17,7 +9,8 @@ def get_springs(filename):
     for line in fh:
         line = line.strip()
         springs, damaged_spring_groups = line.split(' ')
-        rows.append((list(springs), damaged_spring_groups))
+        dsg = tuple([int(x) for x in damaged_spring_groups.split(',')])
+        rows.append((tuple(springs), dsg))
 
     return rows
 
@@ -29,54 +22,47 @@ def parse_spring_groups(springs):
             damaged_group_size += 1
         else:
             if damaged_group_size != 0:
-                result.append(str(damaged_group_size))
+                result.append(damaged_group_size)
                 damaged_group_size = 0
 
     if damaged_group_size > 0:
-        result.append(str(damaged_group_size))
+        result.append(damaged_group_size)
 
-    result_str = ','.join(result)
-    return result_str
+    return result
 
-def determine_valid(springs, damaged_spring_groups):
-    result_str = parse_spring_groups(springs)
+# drew inspiration from https://github.com/CalSimmon/advent-of-code/blob/main/2023/day_12/solution.py
+@functools.cache
+def count_valid_possibilities(springs, dsg, group_size=0):
+    if not springs:
+        all_consumed = not dsg and not group_size
+        can_consume_last = len(dsg) == 1 and dsg[0] == group_size
+        return all_consumed or can_consume_last
 
-    return result_str == damaged_spring_groups
-
-@listToTuple
-@cache
-def count_valid_possibilities(springs, damaged_spring_groups):
-    try:
-        qmark_pos = springs.index('?')
-    except ValueError:
-        qmark_pos = -1
-
-    if qmark_pos != -1:
-        valid_combinations = 0
-
-        new_springs = list(springs)
-        new_springs[qmark_pos] = '.'
-        valid_combinations += count_valid_possibilities(new_springs, damaged_spring_groups)
-
-        new_springs = list(springs)
-        new_springs[qmark_pos] = '#'
-        valid_combinations += count_valid_possibilities(new_springs, damaged_spring_groups)
-
-        return valid_combinations
-    else:
-        return parse_spring_groups(springs) == damaged_spring_groups
+    symbols = ['#', '.'] if springs[0] == '?' else springs[0]
+    valid_posibilities = 0
+    for s in symbols:
+        if s == '#':
+            valid_posibilities += count_valid_possibilities(springs[1:], dsg, group_size+1)
+        if s == '.':
+            if group_size != 0:
+                if dsg and dsg[0] == group_size:
+                    valid_posibilities += count_valid_possibilities(springs[1:], dsg[1:])
+            else:
+                valid_posibilities += count_valid_possibilities(springs[1:], dsg)
+    
+    return valid_posibilities
 
 def unfold(springs):
     unfolded_springs = []
     for springs, damaged_spring_groups in springs:
-        unfolded_spring = list('?'.join([''.join(springs)]*5))
-        unfolded_dsg = ','.join([damaged_spring_groups]*5)
-        print(unfolded_dsg)
+        unfolded_spring = tuple('?'.join([''.join(springs)]*5))
+        unfolded_dsg = damaged_spring_groups*5
         unfolded_springs.append((unfolded_spring, unfolded_dsg))
 
     return unfolded_springs
 
-springs = unfold(get_springs('input_test.txt'))
+# springs = unfold(get_springs('input_test.txt'))
+springs = unfold(get_springs('input.txt'))
 # springs = get_springs('input_test.txt')
 # springs = get_springs('input.txt')
 
