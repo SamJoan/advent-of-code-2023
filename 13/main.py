9c521e3e4649b2ce86921449abe97e9fce0ab524
Sam Roque-Worcel <sam@roque-worcel.com>
2023-12-14 10:08:07 +0000

    qwfqwf

--- 13/main.py
+++ 13/main.py
@@ -22,68 +22,141 @@ def parse_patterns(filename):
 
     return patterns
 
-def identify_mirror_h(pattern):
+def identify_mirror_h(pattern, smudge_pos=None):
     for pos in range(len(pattern[0])-1):
         all_equal = True
-        for line in pattern:
+        nb_diff = 0
+        last_eq_diff_pos = None
+        for p_pos, line in enumerate(pattern):
             next_pos = pos+1
             if line[pos] != line[next_pos]:
                 all_equal = False
-                continue
+                nb_diff += 1
+                last_eq_diff_pos = [pos, (p_pos, pos), (p_pos, next_pos)]
 
-        if all_equal:
-            print('lelele', pos)
+        if all_equal or nb_diff == 1:
             orig_pos = pos
+            print("eql %s" % pos)
+            nb_different = 0
+            all_equal = True
+            last_diff_pos = None
             while True:
                 pos = pos-1
+                # print(pos, next_pos, nb_different)
                 next_pos = next_pos+1
-                all_equal = True
                 try:
                     if pos == -1:
                         raise IndexError
 
-                    for line in pattern:
+                    for p_pos, line in enumerate(pattern):
                         if line[pos] != line[next_pos]:
                             all_equal = False
+                            nb_different += 1
+                            # print(pos, next_pos, nb_different)
+                            last_diff_pos = [orig_pos, (p_pos, pos), (p_pos, next_pos)]
                 except IndexError:
-                    return orig_pos + 1
+                    if all_equal:
+                        if nb_diff == 0 and smudge_pos == orig_pos:
+                            return orig_pos + 1, None
+                        if nb_diff == 1:
+                            return -1, last_eq_diff_pos
+                        else: 
+                            break
+                    else:
+                        break
                 
-                if not all_equal:
-                    break
+            if not all_equal:
+                if nb_different == 1:
+                    pos, option1, option2 = last_diff_pos
+                    print "found smudge!! :((((( %s, %s, %s" % (pos, option1, option2)
+                    p(pattern)
+                    return -1, last_diff_pos
 
+    return 0, None
 
-    return 0
-
-def identify_mirror_v(pattern):
+def identify_mirror_v(pattern, smudge=True):
     flipped = []
-    for row_pos in range(len(pattern[0])):
+    for y in range(len(pattern)):
         row = []
 
-        for pattern_pos in range(len(pattern)):
-            row.append(pattern[pattern_pos][row_pos])
+        for x in range(len(pattern)):
+            row.append(pattern[x][y])
 
         flipped.append(row)
     
     flipped_tuple = convert(flipped)
-    print "flipped"
+    print "FLIPPED"
     p(flipped_tuple)
-    return identify_mirror_h(flipped_tuple)
+    assert pattern[0][0] == flipped_tuple[0][0]
+    assert pattern[1][0] == flipped_tuple[0][1]
+    assert pattern[2][0] == flipped_tuple[0][2]
+    assert pattern[3][0] == flipped_tuple[0][3]
+
+    return identify_mirror_h(flipped_tuple, smudge)
 
 def p(pattern):
     for line in pattern:
         print ''.join(line)
+
+def flip_tuple_coord(pattern, smudge_coords):
+    p_pos, pos = smudge_coords
+    new = []
+    for y, row in enumerate(pattern):
+        new_row = []
+        for x, c in enumerate(row):
+            if y == p_pos and x == pos:
+                new_c = '#' if c == '.' else '.'
+                new_row.append(new_c)
+            else:
+                new_row.append(c)
+
+        new.append(new_row)
+
+    new_tuple = convert(new)
+    return new
+            
 
 # patterns = parse_patterns('input_test.txt')
 patterns = parse_patterns('input.txt')
 
 score = 0
 for pattern in patterns:
-    h_score = identify_mirror_h(pattern)
-    v_score = identify_mirror_v(pattern)
+    found_smudge = False
+    p(pattern)
+    h_score, smudge_coords_list = identify_mirror_h(pattern)
+    print("h_score", h_score)
+    if h_score == -1:
+        smudge_pos = smudge_coords_list.pop(0)
+        for smudge_coords in smudge_coords_list:
+            found_smudge = True
+            pattern = flip_tuple_coord(pattern, smudge_coords)
+            h_score, smudge_coords = identify_mirror_h(pattern, smudge_pos=smudge_pos)
+            print("hscore %s %s" % (h_score, smudge_coords))
+            v_score = 0
+            if h_score > 0:
+                break
+    else:
+        v_score, smudge_coords_list = identify_mirror_v(pattern)
+        print("v_score '%s'" % (v_score))
+        if v_score == -1:
+            smudge_pos = smudge_coords_list.pop(0)
+            for smudge_coords in smudge_coords_list:
+                found_smudge = True
+                p_pos, pos = smudge_coords
+                pattern = flip_tuple_coord(pattern, (pos, p_pos))
+                h_score = 0
+                v_score, smudge_coords = identify_mirror_v(pattern, smudge=smudge_pos)
+                print("v_score %s %s" % (v_score, smudge_coords))
+                if v_score > 0:
+                    break
+
+    if not found_smudge:
+        print "didn't find smudge."
+        sys.exit()
+
     score += h_score
     score += v_score*100
     
-    p(pattern)
     print('rowscore', h_score, v_score)
     
     # raw_input()
