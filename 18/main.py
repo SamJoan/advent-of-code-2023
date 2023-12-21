f4ae9328db163b49a7089dfb7fb48d1d579636ee
Sam Roque-Worcel <sam@roque-worcel.com>
2023-12-21 09:21:58 +0000

    solve with math :(

--- 18/main.py
+++ 18/main.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 def get_positions(filename):
     fh = open(filename)
-    orig_pos = (1, 1)
+    orig_pos = (0, 0)
     positions = [orig_pos]
     x, y = orig_pos
     for line in fh:
@@ -26,7 +26,6 @@ def get_positions(filename):
 
     return positions
 
-
 def get_area(positions):
     area = 0
     for nb, pos in enumerate(positions):
@@ -37,13 +36,37 @@ def get_area(positions):
             next_x, next_y = positions[0]
 
         shoelace = (x * next_y) + (y * next_x * -1)
-        print(shoelace)
         area += shoelace
 
-    return area
+    return area / 2
 
-positions = get_positions("input_test.txt")
-print(positions)
+def get_perimeter(positions):
+    perimeter = 0
+    for nb, pos in enumerate(positions):
+        x, y = pos
+        try:
+            next_x, next_y = positions[nb + 1]
+        except IndexError:
+            next_x, next_y = positions[0]
+
+        perimeter += abs(x - next_x)
+        perimeter += abs(y - next_y)
+
+    return perimeter
+
+def pm(positions):
+    max_y = max(positions, key=lambda x: x[1])[1]
+    max_x = max(positions, key=lambda x: x[0])[0]
+    for y in range(max_y + 1):
+        res = ""
+        for x in range(max_x + 1):
+            if (x, y) in positions:
+                res += '#'
+            else:
+                res += '.'
+        
+# positions = get_positions("input_test.txt")
+positions = get_positions("input.txt")
 # positions = [
     # (1, 6),
     # (3, 1),
@@ -51,6 +74,11 @@ print(positions)
     # (4, 4),
     # (8, 5)
 # ]
-area = get_area(positions)
-print(area)
+# pm(positions)
+area = get_area(positions) # shoelace
+perimeter = get_perimeter(positions) # chaos
+#picks theorem
+
+result = area + (perimeter / 2) + 1
+print(result)
 
