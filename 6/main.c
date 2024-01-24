a9876834aa3d91ef4cd258cbc1117ec1fa6c932e
Sam Roque-Worcel <sam@roque-worcel.com>
2024-01-24 08:49:14 +0000

    Fix tpl issue.

---
+++ 6/main.c
@@ -0,0 +1,42 @@
+#define _GNU_SOURCE
+#include "main.h"
+#include "main_test.h"
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        fprintf(stderr, "Usage: %s (test) <file>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (strcmp(argv[1], "exec") == 0) {
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+        long int result = 0;
+
+	fp = fopen("input.txt", "r");
+	if (fp == NULL)
+	    exit(EXIT_FAILURE);
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+            /*long int calibration_value = get_calibration_value(line);*/
+            /*result += calibration_value;*/
+	}
+
+	fclose(fp);
+	if (line)
+	    free(line);
+
+        /*printf("Final result: %ld\n", result);*/
+
+	exit(EXIT_SUCCESS);
+    } else {
+        run_tests();
+    }
+}
